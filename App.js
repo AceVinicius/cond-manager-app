266609ca7cc5036faa6f5deb17bc9cbc093e8db1
Vinícius Ferreira Aguiar <acevinicius@icloud.com>
2022-06-24 01:10:28 -0300

    Standarized javascript code with eslint

--- App.js
+++ App.js
@@ -17,8 +17,8 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 const Section = ({children, title}) => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -82,7 +82,7 @@ const HomeScreen = () => {
       </ScrollView>
     </SafeAreaView>
   );
-}
+};
 
 const Stack = createNativeStackNavigator();
 
